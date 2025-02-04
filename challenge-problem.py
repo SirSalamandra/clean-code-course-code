059a57d29632d1cc68da87261ccc767f4260a0d3
João Silva <zxjoaozx@gmail.com>
2025-02-04 19:08:19 -0300

    my attempt to fix comments and formatting

--- challenge-problem.py
+++ challenge-problem.py
@@ -1,28 +1,18 @@
 # (c) Maximilian Schwarzmüller / Academind GmbH
 
-# *********
-# Imports
-# *********
 from os import path, makedirs
 from pathlib import Path
-
-# *********
-# Main
-# *********
-# A class which allows us to create DiskStorage instances
-
 
 class DiskStorage:
     def __init__(self, directory_name):
         self.storage_directory = directory_name
-
-    def get_directory_path(self):
-        return Path(self.storage_directory)
-
-    # This must be called before a file is inserted
+        
     def create_directory(self):
         if (not path.exists(self.get_directory_path())):
             makedirs(self.storage_directory)
+
+    def get_directory_path(self):
+        return Path(self.storage_directory)
 
     # Warning: Directory must exist in advance
     def insert_file(self, file_name, content):
@@ -33,5 +23,4 @@ class DiskStorage:
 
 
 log_storage = DiskStorage('logs')
-
 log_storage.insert_file('test.txt', 'Test')
